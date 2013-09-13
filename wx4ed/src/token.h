814c5a7372ba1ccdbc687e25407118143ab48dd0
Weilin <mzweilin@gmail.com>
2013-09-13 16:16:06 -0400

    Forgot to add token.h in the last commit.

--- wx4ed/src/token.h
+++ wx4ed/src/token.h
@@ -8,8 +8,12 @@
 #define STEP 263
 #define TO 264
 #define DO 265
+
+// BEGIN END {}
 #define COPEN 266
 #define CCLOSE 267
+
+// sin cos sqrt
 #define SIN 268
 #define COS 269
 #define SQRT 270
@@ -17,13 +21,41 @@
 #define ID 272
 #define NUMBER 273
 #define SEMICOLON 274
+
+// +-*/
 #define PLUS 275
 #define MINUS 276
 #define TIMES 277
 #define DIV 278
+
+// ()
 #define OPEN 279
 #define CLOSE 280
+// =
 #define ASSIGN 281
+
+#define UP 282
+#define DOWN 283
+
+#define NORTH 284
+#define EAST 285
+#define WEST 286
+#define SOUTH 287
+ 
+#define IF 288
+#define THEN 289
+#define ELSE 290
+
+#define GREATER 291
+#define LESS 292
+#define EQUAL 293
+#define GE 294
+#define LE 295
+
+#define WHILE 296
+
+#define PROCEDURE 297
+#define COMMA 298
 
 
 
